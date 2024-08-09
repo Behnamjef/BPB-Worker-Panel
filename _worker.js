c415f9f06990bb915f3e869945119aaf22507398
BehnamJef <behnamjaffarii@gmail.com>
2024-08-09 19:12:58 +0330

    Update userID

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '55378238-007b-4bfc-943a-5047bb37b27e';
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
 const proxyIPs= ['bpb.yousef.isegaro.com'];
